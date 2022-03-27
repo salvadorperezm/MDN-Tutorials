92f78b47fc493d1ff794c6a7e1617aaff4f3274f
Salvador Perez <salvadorperezm115@gmail.com>
2022-03-26 18:48:04 -0600

    adding files

---
+++ javascript/building-blocks/functions/js/task-four.js
@@ -0,0 +1,15 @@
+const names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert', 'Jada'];
+const para = document.createElement('p');
+
+function isShort(name) {
+    return name.length < 5;
+}
+
+const shortNames = names.filter(isShort);
+para.textContent = shortNames;
+
+// Don't edit the code below here!
+
+const section = document.querySelector('section');
+
+section.appendChild(para);
---
+++ javascript/building-blocks/functions/js/task-three.js
@@ -0,0 +1,10 @@
+const names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert', 'Jada']
+const para = document.createElement('p');
+
+// Add your code here
+
+// Don't edit the code below here!
+
+const section = document.querySelector('section');
+
+section.appendChild(para);

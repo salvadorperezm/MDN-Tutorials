fac4588539207043c34e977d9e8fd554f2a5f342
Salvador Perez <salvadorperezm115@gmail.com>
2022-03-27 22:13:31 -0600

    updating files

--- javascript/building-blocks/functions/js/task-one.js
+++ javascript/building-blocks/functions/js/task-one.js
@@ -2,6 +2,15 @@ const names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert
 const para = document.createElement('p');
 
 // Add your code here
+const button = document.querySelector(".button");
+function chooseName() {
+    let randomNumber = Math.floor(Math.random() * (names.length) ) + 0;
+    para.textContent = `Your random name is: ${names[randomNumber]}.`;
+}
+
+chooseName();
+
+button.addEventListener("click", chooseName);
 
 // Don't edit the code below here!
 
--- javascript/building-blocks/functions/js/task-three.js
+++ javascript/building-blocks/functions/js/task-three.js
@@ -2,7 +2,19 @@ const names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert
 const para = document.createElement('p');
 
 // Add your code here
+const button = document.querySelector(".button");
 
+function random(min, max) {
+    return Math.floor(Math.random() * (max) ) + min;
+}
+
+function chooseName() {
+    para.textContent = `Your random name is: ${names[random(0, names.length)]}`;
+}
+
+chooseName();
+
+button.addEventListener("click", chooseName);
 // Don't edit the code below here!
 
 const section = document.querySelector('section');

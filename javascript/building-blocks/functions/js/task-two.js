2ede08514c2c14ea6aec8dedd5914ce7b9d34404
Salvador Perez <salvadorperezm115@gmail.com>
2022-03-28 22:45:48 -0600

    added task 2

--- javascript/building-blocks/functions/js/task-two.js
+++ javascript/building-blocks/functions/js/task-two.js
@@ -8,6 +8,15 @@ const height = 75;
 const color = 'blue';
 
 // Add your code here
+function draw() {
+  ctx.clearRect(0, 0, width, height);
+  ctx.beginPath();
+  ctx.fillStyle = "blue";
+  ctx.rect(x, y, width, height);
+  ctx.fill();
+}
+
+draw();
 
 // Don't edit the code below here!
 

30e91069be862c984984b98963751e0a171cd154
Salvador Perez <salvadorperezm115@gmail.com>
2022-04-03 19:45:09 -0600

    adding task one

--- javascript/building-blocks/events/js/task-one.js
+++ javascript/building-blocks/events/js/task-one.js
@@ -1,3 +1,16 @@
 const btn = document.querySelector('.off');
 
 // Add your code here
+
+function checkMachine() {
+    switch(btn.textContent) {
+        case "Machine is off":
+            btn.textContent = "Machine is on";
+            break;
+        case "Machine is on":
+            btn.textContent = "Machine is off";
+            break;
+    }
+}
+
+btn.addEventListener("click", checkMachine);

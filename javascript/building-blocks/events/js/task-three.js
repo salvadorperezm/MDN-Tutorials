ed2bde249f6879682b3307d724a3186b793a3bd0
Salvador Perez <salvadorperezm115@gmail.com>
2022-04-05 23:12:16 -0600

    adding task three

--- javascript/building-blocks/events/js/task-three.js
+++ javascript/building-blocks/events/js/task-three.js
@@ -6,15 +6,13 @@ const yellowButton = document.querySelector("[data-color='yellow']");
 const greenButton = document.querySelector("[data-color='green']");
 const purpleButton = document.querySelector("[data-color='purple']");
 
-const buttons = [redButton, yellowButton, greenButton, purpleButton];
-
 function changeBackground(e) {
-    const color = e.currentTarget.getAttribute("data-color");
-    return color;
+    const color = e.target.getAttribute("data-color");
+    buttonBar.style.backgroundColor = color;
 }
 
-buttonBar.addEventListener("click", e => e.currentTarget.style.backgroundColor = changeBackground());
-// redButton.addEventListener("click", changeBackground);
-// yellowButton.addEventListener("click", changeBackground);
-// greenButton.addEventListener("click", changeBackground);
-// purpleButton.addEventListener("click", changeBackground);
+buttonBar.addEventListener("click", changeBackground);
+redButton.addEventListener("click", changeBackground);
+yellowButton.addEventListener("click", changeBackground);
+greenButton.addEventListener("click", changeBackground);
+purpleButton.addEventListener("click", changeBackground);

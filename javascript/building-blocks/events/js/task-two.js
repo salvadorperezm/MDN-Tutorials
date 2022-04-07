8874d53212dc85be6409565e1126ce11e7deae07
Salvador Perez <salvadorperezm115@gmail.com>
2022-04-06 22:32:58 -0600

    adding task two

--- javascript/building-blocks/events/js/task-two.js
+++ javascript/building-blocks/events/js/task-two.js
@@ -18,3 +18,47 @@ const size = 30;
 drawCircle(x, y, size);
 
 // Add your code here
+const preview = document.querySelector(".preview");
+const userInput = document.createElement("input");
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset";
+
+preview.appendChild(userInput);
+preview.appendChild(resetButton);
+
+function translateCircle(e) {
+    switch(e.key) {
+        case "w":
+            y -= 20;
+            drawCircle(x, y, size);
+            break;
+        
+        case "s":
+            y += 20;
+            drawCircle(x, y, size);
+            break;
+
+        case "a":
+            x -= 20;
+            drawCircle(x, y, size);
+            break;
+        
+        case "d":
+            x += 20;
+            drawCircle(x, y, size);
+            break;
+
+        default:
+            alert("You can only press wasd");
+            break;
+    }
+} 
+
+userInput.addEventListener("keydown", translateCircle);
+
+resetButton.addEventListener("click", () => {
+    userInput.value = "";
+    const xOrigin = 50;
+    const yOrigin = 50;
+    drawCircle(xOrigin, yOrigin, size);
+});

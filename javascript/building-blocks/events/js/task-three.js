d9b4e48ba55c8fcef05288d9970b0c4935269233
Salvador Perez <salvadorperezm115@gmail.com>
2022-04-09 11:33:20 -0600

    updating task 3

--- javascript/building-blocks/events/js/task-three.js
+++ javascript/building-blocks/events/js/task-three.js
@@ -1,18 +1,6 @@
 const buttonBar = document.querySelector('.button-bar');
 
 // Add your code here
-const redButton = document.querySelector("[data-color='red']");
-const yellowButton = document.querySelector("[data-color='yellow']");
-const greenButton = document.querySelector("[data-color='green']");
-const purpleButton = document.querySelector("[data-color='purple']");
-
-function changeBackground(e) {
-    const color = e.target.getAttribute("data-color");
-    buttonBar.style.backgroundColor = color;
-}
-
-buttonBar.addEventListener("click", changeBackground);
-redButton.addEventListener("click", changeBackground);
-yellowButton.addEventListener("click", changeBackground);
-greenButton.addEventListener("click", changeBackground);
-purpleButton.addEventListener("click", changeBackground);
+buttonBar.addEventListener("click", (e) => {
+    e.currentTarget.style.backgroundColor = e.target.textContent;
+});

58ade884dd4be7b5a08f0d7449e7ea0f41fe5685
Salvador Perez <salvadorperezm115@gmail.com>
2022-04-10 11:10:26 -0600

    declaring filenames array

--- javascript/building-blocks/image-gallery/main.js
+++ javascript/building-blocks/image-gallery/main.js
@@ -6,6 +6,8 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
 /* Looping through images */
 
 const newImage = document.createElement('img');
